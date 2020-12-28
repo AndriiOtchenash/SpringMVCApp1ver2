b8ccba78f8fdc58d08347a4621a391c4298cb088
Andrii Otchenash <andrii.otchenash@gmail.com>
2020-12-28 10:35:39 +0200

    GET request parameters

--- src/main/java/com/otchenash/сontrollers/FirstController.java
+++ src/main/java/com/otchenash/сontrollers/FirstController.java
@@ -3,15 +3,27 @@ package com.otchenash.сontrollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequestMapping("/first")
+
 public class FirstController {
 
-    @GetMapping("/hello")
-    public String helloPage(){
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request){
+//        String name = request.getParameter( "name" );
+//        String surname = request.getParameter( "surname" );
+
+        @GetMapping("/hello")
+        //@RequestMapping("/first")
+        public String helloPage(@RequestParam(value = "name", required = false) String name,
+                                @RequestParam("surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
+
 
     @GetMapping("/goodbye")
     public String goodByePage(){
